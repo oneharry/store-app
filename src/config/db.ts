04dcc7d7b4301b64dcc65635977ef22ca08471ce
oneharry <harryofficialchidi@yahoo.com>
2025-01-28 13:08:47 +0100

    fixed bug - product not updating

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
-
 const connectDB = async () => {
   try {
     const encodedPassword = encodeURIComponent(process.env.MONGODB_USER_PASSWORD);
 const { MONGODB_USERNAME, MONGODB_DATABASE, MONGODB_CLUSTER } = process.env;
 
     const url = `mongodb+srv://${MONGODB_USERNAME}:${encodedPassword}@${MONGODB_CLUSTER}/${MONGODB_DATABASE}?retryWrites=true&w=majority`
-
-    await mongoose.connect(url);
+    const ur = 'mongodb://mongodb:27017/storeapi';
+    console.log("ENV URL", url);
+    await mongoose.connect(ur);
 
     console.log('Database connection established');
   } catch (error) {
